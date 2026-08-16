2bde6a9fda25ed578bd31adefd6bb353e832d8e5
DeMoXoNiX <149050107+griaa125@users.noreply.github.com>
2026-08-17 01:06:40 +0600

    Delete tunerift-full-working-v20 directory

--- tunerift-full-working-v20/src/settingsPreload.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('api', {
-  listAddons: () => ipcRenderer.invoke('addons:list'),
-  toggleAddon: (id, enabled) => ipcRenderer.invoke('addons:toggle', id, enabled),
-  getSettings: () => ipcRenderer.invoke('settings:get'),
-  setSetting: (key, value) => ipcRenderer.invoke('settings:set', key, value)
-});
